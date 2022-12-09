0c6bde6d431bbc2dac46d0e3f984d6bc56e821e1
Demian Tetteh <demiantetteh@gmail.com>
2022-12-09 12:35:24 -0800

    Advanced tasks completed with help nothing I did was my crap

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	int y;
+
+	for (x = 0; x < 9; x ++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
