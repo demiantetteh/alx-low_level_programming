2c81311af3e4c005b2147a145c207e2d59a62c08
Demian Tetteh <demiantetteh@gmail.com>
2022-12-08 23:17:03 -0800

    Fixed error in task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu.\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu.\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(long int));
-	printf("Size of long long int: %lu.\n", (unsigned long)sizeof(long long int));
-	printf("Size of a float: %lu.\n", (unsigned long)sizeof(float));
+	printf("Size of a char: %lu byte(s).\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s).\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu bytes(s).\n", (unsigned long)sizeof(long int));
+	printf("Size of long long int: %lu byte(S).\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s).\n", (unsigned long)sizeof(float));
 
 	return (0);
 }
