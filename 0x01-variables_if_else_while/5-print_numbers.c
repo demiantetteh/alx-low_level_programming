ac27705f374e2c79600e2113bde85610c0a06799
Demian Tetteh <demiantetteh@gmail.com>
2022-12-09 10:00:09 -0800

    Courage

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - printing numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	for (x = '0'; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
