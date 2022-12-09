8a1df26bc4e372f85a9fdeeb6b7ca1fa0de3e23a
Demian Tetteh <demiantetteh@gmail.com>
2022-12-09 08:44:13 -0800

    Getting the hang of no help

---
+++ 0x01-variables_if_else_while/2-print_alpahabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int C;
+	for (C = 'a'; C <= 'z'; C++)
+	{
+		putchar(C);
+	}
+	return (0);
+}
