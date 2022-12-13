6d2ec389edecd8926cb905aff78c213838cc1bb4
Demian Tetteh <demiantetteh@gmail.com>
2022-12-13 05:04:38 -0800

    Prints _putchar and ends with a newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - prints the word _putchar \n
+ *
+ * Return - Always 0 (success)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar \n";
+	int i = 0;
+
+	while(int i != 0)
+	{
+		char c = word[i];
+
+		_putchar.c;
+		i++;
+	}
+	return (0);
+}
