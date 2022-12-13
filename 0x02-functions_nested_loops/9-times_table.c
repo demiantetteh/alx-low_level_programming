b758403b1f19356df90f668a0bb43e083d50c244
Demian Tetteh <demiantetteh@gmail.com>
2022-12-13 09:23:07 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,42 @@
 #include "main.h"
+
 /**
- * times_table - Prints the 9 times table, starting with 0.
- * 
- * Return: void.
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
  */
+
 void times_table(void)
 {
-	int num, mult, prod;
+	int row, column, product, tens, ones;
 
-	for (num - 0; num <= 9; num++)
+	for (row = 0; row <= 9; row++)
 	{
-		_putchar(',');
-		_putchar(' ');
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-		prod = num * mult;
-
-		if (prod <= 9)
-			_putchar(' ');
-		else
-			_putchar((prod / 10) + '0');
-
-		_putchar((prod % 10) + '0');
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
