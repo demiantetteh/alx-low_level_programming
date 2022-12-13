1acff23e738a14165ea07462cad2310d2fa06540
Demian Tetteh <demiantetteh@gmail.com>
2022-12-13 10:33:18 -0800

    Advanced Tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,13 +11,15 @@ int main(void)
 {
 	int i, sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	while (i < 1024)
 	{
-		if ((i % 3) == 0 || (i % 5) == 5)
-			sum += i;
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i
+		}
+		i++;
 	}
 
 	printf("%d\n", sum);
-
 	return (0);
 }
