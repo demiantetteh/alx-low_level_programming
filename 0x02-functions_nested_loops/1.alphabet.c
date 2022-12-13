e3c250d9f2809c56af027f6888e09d9bf49c07ca
Demian Tetteh <demiantetteh@gmail.com>
2022-12-13 05:59:56 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alpahabet - entry point
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n')
+}
