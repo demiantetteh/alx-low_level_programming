5e57e65230f39bfa0d96b40710c8e0a467d75cb8
demiantetteh <118938327+demiantetteh@users.noreply.github.com>
2022-12-13 14:15:15 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -15,5 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
-
-#endif
