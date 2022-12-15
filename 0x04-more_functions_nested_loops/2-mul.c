86f6c04fffaee57bfce9f4a309276df29ff202fd
Demian Tetteh <demiantetteh@gmail.com>
2022-12-15 12:32:14 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - checks the code to multiply two integers
+ * @a: is the integer variable involved in the multiplication
+ * @b: is the secont integer variable involved in the multiplication
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
