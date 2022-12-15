aa2b8ab1a5795833ade6730ae5faf6b0890043e0
Demian Tetteh <demiantetteh@gmail.com>
2022-12-15 12:46:17 -0800

    Tasks 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 with the
+ * excetion of 2 and 4 followed by a new line
+ * Return: void is success
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	while (a != 2 and a != 4)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
