2079447b77869523f5479d000069a96234748d3b
Demian Tetteh <demiantetteh@gmail.com>
2022-12-15 12:13:24 -0800

    Completed task 0 with ease

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is upper case
+ *@c: is the integer variable
+ *Return - 1 if true and 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	return(0);
+}
