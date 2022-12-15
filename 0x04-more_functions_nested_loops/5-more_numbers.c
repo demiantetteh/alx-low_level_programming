b7152f93251976ff69350a5f2212bd7d453878fd
Demian Tetteh <demiantetteh@gmail.com>
2022-12-15 13:26:12 -0800

    Fixed the error with bo help

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,17 @@
 
 void more_numbers(void)
 {
-	int rows;
+	int rows = 0;
 	int num;
 
-	while (rows <= 13)
+	while (rows <= 9)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			_putchar(num);
 			_putchar('\n');
 		}
+		rows++;
 		_putchar('\n');
 	}
 }
