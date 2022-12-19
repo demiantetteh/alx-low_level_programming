e541624661e8dea8352f15ee368e4a7e497c8dd6
Demian Tetteh <demiantetteh@gmail.com>
2022-12-19 11:32:12 -0800

    Task 8*

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,19 +8,16 @@
 
 void puts_half(char *str)
 {
-	int index = 0, len = 0, n;
+	int i;
 
-	while (str[index++])
-		len++;
-
-	if ((len % 2) == 0)
-		n = len / 2;
-
-	else
-		n = (len + 1) / 2;
-
-	for (index = n; index < len; index++)
-		_putchar(str[index]);
-
-	_putchar('\n');
+	i = 0;
+	for (n--; n >= 0; n--; i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
