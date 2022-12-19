7368ee4fd3bf29626cb6a3d2331d62153b8837d0
Demian Tetteh <demiantetteh@gmail.com>
2022-12-19 10:52:07 -0800

    Task 7*

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: the string reference
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
