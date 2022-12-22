019d3e02b759a3f2a992d3580ef4433e2af46bed
Demian Tetteh <demiantetteh@gmail.com>
2022-12-22 04:04:56 -0800

    Task 3*

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_stcrmp - Compares pointers to two strings
+ * *_strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
