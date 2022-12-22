3fa801fe5ec2b50ccf7ef8fbf0f57d808c9f7a82
Demian Tetteh <demiantetteh@gmail.com>
2022-12-22 03:05:52 -0800

    Task 1*

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * *_strncat - appends n characters from src to dest
@@ -11,7 +10,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
