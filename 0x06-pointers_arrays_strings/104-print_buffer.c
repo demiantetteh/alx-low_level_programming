daf8bf06d80cb7208cf29f0df8de0f653f880b64
Demian Tetteh <demiantetteh@gmail.com>
2022-12-22 10:26:11 -0800

    DEAD SKULL

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,10 @@
-#incluede "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
- * 		the byte position, then showing the hex content,
- * 		then displaying printable characters.
+ *		the byte position, then showing the hex content,
+ *		then displaying printable characters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
