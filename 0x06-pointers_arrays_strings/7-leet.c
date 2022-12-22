8c3f6bbe00efab199ac496a8092d2a0f23182275
Demian Tetteh <demiantetteh@gmail.com>
2022-12-22 09:00:03 -0800

    Task 7*

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,8 +15,8 @@ char *leet(char *str)
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
-			if (str[index1 == leet[index2] ||
-			str[index1] - 32 == leet[index2])
+			if (str[index1] == leet[index2] ||
+					str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
 
