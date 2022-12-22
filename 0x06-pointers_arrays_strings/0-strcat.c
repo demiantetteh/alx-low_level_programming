81cbf0baa60ac7c0374731169e40eb03ec453fa6
Demian Tetteh <demiantetteh@gmail.com>
2022-12-22 02:33:46 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - appends @src to @dest
+ * @src - variable to be appended from
+ * @dest - variable to be appended to
+ * Return: dest (success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
