e6cca8bbc3ceb65f0c60444e6d796d3edf1d2670
Demian Tetteh <demiantetteh@gmail.com>
2023-01-03 07:51:08 -0800

    Task 7 smh :(

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,9 +12,9 @@ void print_chessboard(char (*a)[8])
 	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
 		for (indx2 = 0; indx2 < 8; index2++)
-		{
 			_putchar(a[indx1][indx2]);
-		}
+
+
 		_putchar('\n');
 	}
 }
