047252d9921eaa4111b3e62c32530abfc3083fde
Demian Tetteh <demiantetteh@gmail.com>
2023-01-05 11:42:53 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,8 +14,8 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	else if ((n / 1 == 1))
-		return (1);
+	else if ((n / -1 == -1) || (n / -n == 1))
+		return (0);
 	else if ((n / 1 != n) && (n / n != 1))
 		return (0);
 }
