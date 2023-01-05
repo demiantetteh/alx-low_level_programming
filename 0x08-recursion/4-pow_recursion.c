28db22d98f6d9c89051c1b2d6186f0e7eee9bd90
Demian Tetteh <demiantetteh@gmail.com>
2023-01-05 09:58:21 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of an integer raised
+ * to the power of another integer
+ * @x: The base integer.
+ * @y: The exponent integer.
+ * Return: if y < 0 - -1
+ *	if y == 0 - 0
+ *	value of x raised to y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y - 1));
+}
