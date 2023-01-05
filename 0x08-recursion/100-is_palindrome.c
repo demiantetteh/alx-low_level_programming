9e4140ce01aad72fcc6a86a10c75c11e98ebb079
Demian Tetteh <demiantetteh@gmail.com>
2023-01-05 13:08:33 -0800

    DIFFICULT

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,62 @@
-#include <string.h>
 #include "main.h"
+
+int find_strlen(char *s);
+int check_pal(char *s, int len, int index);
+
+/**
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_pal - Checks if a s string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1
+ *		If the string isn't a palindrome - 0.
+ */
+
+int check_pal(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+	if (s[index] == s[len - index - 1])
+		return (check_pal(s, len, index + 1));
+
+	return (0);
+}
 
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
  *
- * Return: If the string is a palindrome -1.
- *		If the string is not a palindrome - 0.
+ * Return: If the string is a palindrome - 1.
+ *		If the string isn't a palindrome - 0.
  */
 
 int is_palindrome(char *s)
 {
-	if (strrev(*s) == *s)
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
 		return (1);
-	else
-		return (0);
+	return (check_pal(s, len, index));
 }
