955229610693b53c208b9a47267f3aa73ee9aa8c
Demian Tetteh <demiantetteh@gmail.com>
2023-01-09 06:49:55 -0800

    Yeahhhhhh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: Number of arguments used on command line
+ * @argv: Arguments used on command line
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
