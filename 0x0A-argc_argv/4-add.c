dc4f6119dd1f56d3354525fd8d75a36c02f09593
Demian Tetteh <demiantetteh@gmail.com>
2023-01-09 08:40:51 -0800

    GOT HELP FROM AI

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,18 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - adds positive numbers and prints them
+ * @argc: The count of arguments passed
+ * @argv: The arguments passed
+ *
+ * Return: 1 if numbers contains symbols that are non digits
+ *		0 -if otherwise
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
 		printf("0\n");
-		
 		return (0);
 	}
 
 	int n1, n2, sum = 0;
 
-	for (n1 = 1; n1 < argc; n1++);
+	for (n1 = 1; n1 < argc; n1++)
 	{
 		for (n2 = 0; argv[n1][n2]; n2++)
 		{
@@ -23,7 +31,6 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-
 		sum += atoi(argv[num]);
 	}
 
