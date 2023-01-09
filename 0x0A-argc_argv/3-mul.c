f3b3d89b40b9371701db5781ebc53368c5c14c29
Demian Tetteh <demiantetteh@gmail.com>
2023-01-09 07:22:43 -0800

    Understood

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers from console in atoi form
+ * @argc: argument count
+ * @argv: arguments used in console
+ *
+ * Return: 0 if argc is more than 3
+ *		1 if argc is less than 3
+ */
+
+int main(int agrc, char *argc[])
+{
+	int n1, n2, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+
+		product = n1 * n2;
+
+		printf("%d\n", product);
+
+		return (0);
+	}
+}
