b891328322e7d07bf94365a3469c97a4da44beb4
Demian Tetteh <demiantetteh@gmail.com>
2023-01-09 06:51:12 -0800

    Yeahhhh

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,6 +5,8 @@
  * main - Prints the number of arguments
  * @argc: Number of arguments used on command line
  * @argv: Arguments used on command line
+ *
+ * Return: 0 is always success
  */
 
 int main(int argc, char __attribute__ ((unused)) *argv[])
