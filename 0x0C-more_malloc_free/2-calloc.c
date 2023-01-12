3f270cec3cb2b09cd35247be2cea1e4766ed590c
Demian Tetteh <demiantetteh@gmail.com>
2023-01-12 06:36:18 -0800

    Task 2: simple

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _calloc - alloocates memory for an array
+ * @nmemb: the number of elements
+ * @size: the size of bytes.
+ *
+ * Return: NULL if size or nmemb is 0
+ *		NULL if malloc fails.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+
+	s = calloc(nmemb, size);
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	if (s == NULL)
+		return (NULL);
+
+	return (s);
+}
