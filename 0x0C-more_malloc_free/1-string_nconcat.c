85f669a21e76d15e8bfb015e30c4b7eb17b981b8
Demian Tetteh <demiantetteh@gmail.com>
2023-01-12 06:17:33 -0800

    Took sometime frrrr

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,13 +16,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1, i, len2;
 	char *result;
-	len1 = 0, i = 0, len2 =0;
 
+	len1 = 0, i = 0, len2 = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	/* calculating the length of s1 and s2*/
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
@@ -35,7 +34,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 		if (result == NULL)
 			exit(1);
-
 		for (i = 0; i < len1; i++)
 			result[i] = s1[i];
 		for (i = 0; i < len2; i++)
