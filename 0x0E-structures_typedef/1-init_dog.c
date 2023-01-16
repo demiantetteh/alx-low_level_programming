b981a19e5964900bb09dd17c842fa47c000b0bcb
Demian Tetteh <demiantetteh@gmail.com>
2023-01-16 12:00:27 -0800

    Task 1 - initialising struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initialises a variale of type struct dog
+ * @d: The dog to be initialised
+ * @name: Name of the dog to be initialised
+ * @age: Age of the dog to be initialised
+ * @owner: Owner of the dog that was initialised
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
