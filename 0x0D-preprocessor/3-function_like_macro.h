12bae9a0c6307bea3304683f5c583ee39a915774
Demian Tetteh <demiantetteh@gmail.com>
2023-01-16 09:57:19 -0800

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * file: 3-function like macro
+ * Description: computes the absolute value of the functionlike macro
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
