2d609a52516a6d2238063759c6a41e29304c3448
Demian Tetteh <demiantetteh@gmail.com>
2023-01-16 10:35:00 -0800

    Task 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,6 +4,8 @@
 /**
  * File: Header file that defines a fuction-like macro SUM(x, y)
  *	that computes the sum of the numbers x and y
+ * x: The operand to be added y
+ * y: The operand to which x is added to
  */
 
 #define SUM(x, y) ((x) + (y))
