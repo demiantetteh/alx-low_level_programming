60e25ebc24c8def1e03306601311c1dd6068a2ff
Demian Tetteh <demiantetteh@gmail.com>
2023-01-16 09:41:08 -0800

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: Header file that defines a macro
+ *		SIZE as an abbreviation for hte token 1024
+ */
+
+#define SIZE 1024
+
+#endif
