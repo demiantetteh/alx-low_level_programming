a2d35c0429d2a0966fe558946390c3ffe137117b
Demian Tetteh <demiantetteh@gmail.com>
2023-01-19 01:43:41 -0800

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
-
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
