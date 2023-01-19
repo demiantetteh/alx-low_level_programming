b31c9ce1cc3c6f322ddea77c6cc6c61452cf113e
Demian Tetteh <demiantetteh@gmail.com>
2023-01-19 00:50:22 -0800

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+	if (f == NULL || name == NULL)
+		return;
+}
