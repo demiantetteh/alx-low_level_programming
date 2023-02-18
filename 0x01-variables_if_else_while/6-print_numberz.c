9efbc0ab4ced5cff0ac5d4438f7f3b3d2f368832
Demian Tetteh <demiantetteh@gmail.com>
2023-02-18 11:32:51 -0800

    Task 9 + comments for 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,6 +12,15 @@ int main(void)
 	int x;
 
 	for (x = 0; x <= 9; x++)
+		/**
+		 * putchar is unable to read integers
+		 * you can enter most of its values as ascii
+		 * so here we wanted to output the value of x starting from 0
+		 * ascii for 0 is 48
+		 * so its adds the value of x to move to the corresponding
+		 * ascii value to print it out
+		 * VOILA
+		 */
 		putchar(x + '0');
 	putchar('\n');
 
