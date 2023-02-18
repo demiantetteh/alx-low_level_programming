5ee89ebc284436d09c6586a15da8c42f7d4397ad
Demian Tetteh <demiantetteh@gmail.com>
2023-02-18 13:57:01 -0800

    Dead

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: Prints all possible combinations of two two-digit numbers.
+ *		the range is from 0 to 99
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			/*print first number*/
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+
+			/*print separator*/
+			putchar(' ');
+
+			/*print second number*/
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			/*print comma and space if not last combination*/
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
