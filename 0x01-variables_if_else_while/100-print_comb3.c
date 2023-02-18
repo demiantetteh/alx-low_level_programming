52889633d932febb12414f21b34044be8fde1c14
Demian Tetteh <demiantetteh@gmail.com>
2023-02-18 12:30:45 -0800

    Task 100 with some help

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,33 @@
 
 int main(void)
 {
-	int x, y, z;
+	int x, y;
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (y = x + 1; y <= 9; y++)
+			/**
+			 * y = x + 1 to make sure that one number is always
+			 * greater than the other to avoid having a pair
+			 * of same number
+			 */
 		{
-			z = x && y;
-			if (x == y)
-				continue;
-			putchar(z + '0');
-			putchar(',');
-			putchar(' ');
+			putchar(x + '0');
+			putchar(y + '0');
+			/**
+			 * The combination ends at 89 and that's the reason
+			 * for the if statement
+			 * The numbers are 0 to 9. therefore there are 10 x 9 = 90
+			 * combinations including 00. but we are posed to take
+			 * same numbers out. eg 00, 11, 33 etc and that's why
+			 * we set y = x + 1, so that the number would always be
+			 * greater than the other.
+			 */
+			if (x < 8 || y < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
